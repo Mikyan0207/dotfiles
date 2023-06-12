56939d930a90762e666cd84616365bd951677802
Florian Theron <mikyan0207@gmail.com>
2023-06-12 08:54:28 +0200

    Update Microsoft.PowerShell_profile.ps1

--- windows-terminal/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/Microsoft.PowerShell_profile.ps1
@@ -1,86 +1,14 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-function Load-Modules()
-{
-    Load-Module("posh-git");
-    Load-Module("oh-my-posh");
-    Load-Module("VSSetup");
-    Load-Module("Terminal-Icons");
-    Load-Module("PSReadLine");
-    Load-Module("z");
-    # Load-Module("PSFzf");
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\flori\.config\komorebi'
+
+function OverrideLs($args) {
+    lsd --header --color=always --group-directories-first $args
 }
 
-function Configure-Modules()
-{
-    # Load Prompt Config
-    oh-my-posh --init --shell pwsh --config '~/.config/powershell/florian.omp.json' | Invoke-Expression
-    
-    # PSReadLine
-    $PSReadLineOptions = @{
-        EditMode = "Emacs"
-        BellStyle = "None"
-        PredictionSource = "History"
-        HistoryNoDuplicates = $true
-        HistorySearchCursorMovesToEnd = $true
-        Colors = @{
-            "InlinePrediction" = "#B6B8BA"
-        }
-    }
+Set-Alias -Name 'ls' -Value OverrideLs -Force -Option AllScope
+Set-Alias -Name 'cat' -Value 'bat' -Force -Option AllScope
 
-    Set-PSReadLineOption @PSReadLineOptions
+New-Alias -Name 'k' -Value 'komorebic' -Force -Option AllScope
 
-    # fzf
-    # Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-}
-
-Load-Modules
-Configure-Modules
-
-# Alias
-Set-Alias -Name ll -Value ls
-Set-Alias -Name g -Value git
-Set-Alias -Name cl -Value clear
-Set-Alias -Name gs -Value GitStatus
-Set-Alias -Name c -Value OpenCode
-Set-Alias -Name vs -Value OpenVS
-
-# Utilities
-function which ($command)
-{
-    Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-function GitStatus { git status }
-
-function OpenCode { code $args }
-
-function OpenVS($solution)
-{
-    $vsPath = Get-VSSetupInstance | Select-Object -ExpandProperty InstallationPath
-    if ([string]::IsNullOrEmpty($vsPath)) { return }
-    $solutionPath = Resolve-Path -LiteralPath $solution
-    & "$vsPath\Common7\IDE\devenv.exe" "$solutionPath"
-}
-
-function Load-Module($m)
-{
-    if (!(Get-Module | Where-Object {$_.Name -eq $m}))
-    {
-        # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m})
-        {
-            Import-Module $m -Verbose
-        }
-        else
-        {
-            # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m})
-            {
-                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-                Import-Module $m -Verbose
-            }
-        }
-    }
-}
+Invoke-Expression (&starship init powershell)
