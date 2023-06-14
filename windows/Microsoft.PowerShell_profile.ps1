e7d7752fd7da082828ed52c462eae71328a03b42
Florian Theron <mikyan0207@gmail.com>
2023-06-14 10:22:24 +0200

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,23 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\flori\.config\komorebi'
-$Env:USER = 'mikyan'
+$Env:USER = '<YOUR_NAME>'
 
 function OverrideLs($args) {
     lsd --header --color=always --group-directories-first $args
 }
 
+function StartGlazeWM() {
+    Start-Process -NoNewWindow glazewm.exe --config="C:\Users\<REPLACE_WITH_USER>\.config\glazewm.yaml"
+}
+
+function StopGlazeWM() {
+    Stop-Process -Name 'glazewm'
+}
+
+Set-Alias -Name 'wm' -Value StartGlazeWM -Force -Option AllScope
+Set-Alias -Name 'wm stop' -Value StopGlazeWM -Force -Option AllScope
+
 Set-Alias -Name 'ls' -Value OverrideLs -Force -Option AllScope
 Set-Alias -Name 'cat' -Value 'bat' -Force -Option AllScope
-
-New-Alias -Name 'k' -Value 'komorebic' -Force -Option AllScope
 
 Invoke-Expression (&starship init powershell)
